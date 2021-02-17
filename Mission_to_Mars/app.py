9ec1ec61f51c7d3d9c975c1c13ad234ca2f732e4
AmyWagarCinch <gretylschmetyl@gmail.com>
2021-02-17 18:09:04 -0500

    routes

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    destination_data = mongo.db.mars.find_one()
 
     # Return template and data
     return render_template("index.html", mars=destination_data)
@@ -25,10 +25,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.scrape_all()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.mars.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
